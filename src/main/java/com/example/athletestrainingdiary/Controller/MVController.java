31ac8e8c58f39362a545aea0331db2067aa57be4
Astrekcz <vi10101050@gmail.com>
2024-07-10 16:41:21 +0200

    css wip

--- src/main/java/com/example/athletestrainingdiary/Controller/MVController.java
+++ src/main/java/com/example/athletestrainingdiary/Controller/MVController.java
@@ -1,15 +1,17 @@
 package com.example.athletestrainingdiary.Controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class MVController {
 
-    @GetMapping("index")
+    @RequestMapping("wb")
     public String LandingPage(){
-        return "index";
+        return "wb";
     }
 
 }
